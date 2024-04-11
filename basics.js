0bda54e7b10bad1088257ab434744d5bdce9ad1d
asathyjith <asathyajith98@gmail.com>
2024-04-11 18:27:58 +0530

    initial commit

---
+++ basics.js
@@ -0,0 +1,48 @@
+//lifecylce methods
+
+
+//conditional rendering
+
+const MyComponent = ({ isLoggedIn }) => {
+    return (
+      <div>
+        {isLoggedIn ? <LoggedInComponent /> : <LoginComponent />}
+      </div>
+    );
+  };
+  
+  const AndComponent = ({ isLoggedIn }) => {
+    return (
+      <div>
+        {isLoggedIn && <LoggedInComponent />}
+      </div>
+    );
+  };
+  
+  const IfComponent = ({ isLoggedIn }) => {
+    if (isLoggedIn) {
+      return <LoggedInComponent />;
+    } else {
+      return <LoginComponent />;
+    }
+  };
+  
+  //styling in react JS
+  ///1st method 
+  const styledCompo = () => {
+      const xStyle = {
+          color: 'green',
+          fontSize: '10px',
+      };
+      return <div style={xStyle}>hello world</div>;
+  };
+  /// 2nd method
+  //   .abc(
+  //     text-colur:green;
+  //   )  
+  const abc=()=>{
+  return(xyc?<div className="abc">hello world</div>:"");
+  };
+    
+  //HTTP requests in react js  
+  const c = axios.post("URL",{}).then().catch();
