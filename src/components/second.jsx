680f24c1c7f86fef57e8a26d38b4cf7a56405365
asathyajith <asathyajith0521@gmail.com>
2026-07-16 13:17:30 +0530

    Add car function

--- src/components/second.jsx
+++ src/components/second.jsx
@@ -5,20 +5,25 @@ import Car from "./cars";
 import useList from "../hooks/useList";
 
 function CustomHookComp() {
-  const initialState = [
+
+  const [cars, setCars] = useState([
     { name: "benz", rpm: 10000 },
     { name: "audi", rpm: 5000 },
     { name: "mahindra", rpm: 3000 },
-  ];
-
-  const items = useList(initialState);
+  ]);
   const [editable, setEditable] = useState(false);
-  
+  const [newCar, setNewCar] = useState({ name: "", rpm: 0 });
+  const handleAddCar = () => {
+    if (newCar.name && newCar.rpm) {
+      setCars([...cars, newCar]);
+      setNewCar({ name: "", rpm: 0 });
+    }
+  }
   const removeLowHandle = () => {
-    items.removeParent();
+    setCars(cars.filter((car) => car.rpm > 4000));
   };
   const removeInChildHandle = (e) => {    
-    items.removeChild(e.target.name);       
+    setCars(cars.filter((car) => car.name !== e.target.name));       
   };
   const makeEditableHandle = () => {
     setEditable(true);
@@ -26,7 +31,7 @@ function CustomHookComp() {
   const keyPressHandle = (e, i) => {
     if (e.key === "Enter") {
       setEditable(false);
-      items.saveItem(i,e.target.value)
+      cars.saveItem(i,e.target.value)
     }
   };
 
@@ -34,7 +39,7 @@ function CustomHookComp() {
     <div className="App">
       <header className="App-header">
       <h2>Car List</h2>
-        {items.list.map((v, k) => {
+        {cars.map((v, k) => {
           return (
             <Car
               key={`${k}${v.name}${v.rpm}`}
@@ -47,6 +52,19 @@ function CustomHookComp() {
             ></Car>
           );
         })}
+        <form>
+          <fieldset>
+            <legend>Car Details</legend>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" onChange={(e) => setNewCar({...newCar, name: e.target.value})} />
+            <br />
+            <label htmlFor="rpm">RPM:</label>
+            <input type="number" id="rpm" name="rpm" onChange={(e) => setNewCar({...newCar, rpm: parseInt(e.target.value)})} />
+          </fieldset>
+        </form>
+        <button onClick={handleAddCar} className="add-button">
+          Add Car
+        </button>
         <button onClick={removeLowHandle} className="remove-button">
           Remove low RPM
         </button>
